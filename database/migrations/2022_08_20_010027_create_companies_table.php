b0ba297bb96bc20096a74a6264665e446265f16b
Bolaji Mohammed Kolawole <kbolaji80@yahoo.com>
2022-09-04 23:16:17 +0100

    main

--- database/migrations/2022_08_20_010027_create_companies_table.php
+++ database/migrations/2022_08_20_010027_create_companies_table.php
@@ -15,13 +15,13 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            $table->foreignId('user_id')->references('id')->on('users');
             $table->string('name');
             $table->string('email');
             $table->string('address');
             $table->decimal('latitude', 10,8);
             $table->decimal('longitude', 11,8);
             $table->string('phone');
+            $table->foreignId('created_by')->references('id')->on('users');
             $table->boolean('status', 1)->default(0);
             $table->timestamps();
         });
